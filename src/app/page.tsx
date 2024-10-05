3f207a1433f01b4291c72a7083f8ea1d6631d4fd
Allyson Quilter <allysonquilter@gmail.com>
2024-10-05 09:05:15 -0600

    added minimal home page ui to test shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '../components/ui/button';
+
+import { useState, useEffect } from 'react';
+
+
+
 export default function Home() {
   return (
     <main>
-      HOME
+      <Button variant={"secondary"}> Button</Button>
     </main>
   )
 }
